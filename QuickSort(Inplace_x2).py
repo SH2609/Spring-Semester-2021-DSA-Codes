ed5a4894385c602253b702d0bcf5b01a5dc2b8c5
S.M. Safi Haider Rizvi <75332082+SH2609@users.noreply.github.com>
2021-05-25 19:21:48 +0500

    Add files via upload

---
+++ QuickSort(Inplace_x2).py
@@ -0,0 +1,18 @@
+lst = ['T','E','L','E','P','A','T','H','Y']
+def partition(A, start, end):
+    pivot, pindex = A[end], start
+    for i in range(start, end):
+        if A[i] <= pivot:
+            A[i], A[pindex] = A[pindex], A[i]
+            pindex += 1
+    A[end], A[pindex] = A[pindex], A[end]
+    return pindex
+
+def quickSort(A, start, end):
+    if start >= end:
+        return
+    pIndex = partition(A, start, end)
+    quickSort(A, start, pIndex - 1)
+    quickSort(A, pIndex + 1, end)
+    return A
+print(quickSort(lst, start = 0, end = len(lst)-1))
